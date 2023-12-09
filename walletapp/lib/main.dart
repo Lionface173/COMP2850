aaf0b48a5054a19a1b2234c92836414e8d8a07aa
Lionface173 <150566591+Lionface173@users.noreply.github.com>
2023-12-08 22:14:28 -0500

    walletapp

---
+++ walletapp/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:walletapp/models/account.dart';
+import 'package:walletapp/home.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(AccountAdapter());
+  await Hive.openBox('accountsBox');
+
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(colorSchemeSeed: Colors.blue.shade900),
+      title: 'Wallet App',
+      home:  Home(),
+    );
+  }
+}
